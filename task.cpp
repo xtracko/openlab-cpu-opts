4fffea6f4c79d40768b6ab583bb51083c97cc843
Jiří Novotný <novotng@gmail.com>
2015-10-23 00:06:25 +0200

    Mockup for task

--- task.cpp
+++ task.cpp
@@ -1,18 +1,35 @@
-int main() {
-    return 0;
-}
+#include <iostream>
 
 const int size = 100;
 
+struct Vector {
+    int m[size];
+};
 
 struct Matrix {
     int m[size][size];
 };
 
-int transpose(Matrix mat)
+Vector multiply( Matrix mat, Vector vec )
 {
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++)
-            std::cout << mat.m[j][i];
-    }
+    Vector res;
+    for ( int i = 0; i < size; i++ )
+        for ( int j = 0; j < size; j++ )
+            res.m[i] += mat.m[j][i] * vec.m[j];
+    return res;
+}
+
+
+int main()
+{
+    Matrix mat;
+    Vector vec;
+
+    Vector res = multiply(mat, vec);
+
+    for ( int i = 0; i < size; i++ )
+        std::cout << res.m[i];
+    std::cout << std::endl;
+
+    return 0;
 }
