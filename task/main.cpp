15cf5ad80d6eb54290a43d2fd4a6e1a92229d9dd
Jiří Novotný <novotng@gmail.com>
2015-10-23 09:56:08 +0200

    update task

--- task/main.cpp
+++ task/main.cpp
@@ -1,16 +1,60 @@
 #include "task.hpp"
 #include <iostream>
+#include <cstring>
 
-int main()
+void some_func(int option);
+
+int main(int argc, char** argv)
 {
     Matrix mat;
     Vector vec;
 
+    //////////////////////////////////
+    // task one
+    //////////////////////////////////
     Vector res = multiply(mat, vec);
 
     for ( int i = 0; i < size; i++ )
         std::cout << res.m[i];
     std::cout << std::endl;
 
+    /////////////////////////////////
+    // task two
+    /////////////////////////////////
+    task_two(4);
+
+    /////////////////////////////////
+    // task thre
+    /////////////////////////////////
+    task_three(argc, argv);
+
+
     return 0;
+}
+
+void task_two(int option)
+{
+    if (option == 0)
+        std::cout << "fart" << std::endl;
+    else if (option == 1)
+        std::cout << "eat" << std::endl;
+    else if (option == 2)
+        std::cout << "sleep" << std::endl;
+    else if (option == 3)
+        std::cout << "bake" << std::endl;
+    else
+        std::cout << "code" << std::endl;
+}
+
+void task_three(int argc, char** argv) {
+    while (argc > 0) {
+        if (std::strcmp(argc, "help") == 0)
+            std::cout << "Haha" << std::endl;
+        else if (std::strcmp(argc, "work") == 0)
+            std::cout << "No!" << std::endl;
+        else if (std::strcmp(argc, "sleep") == 0)
+            std::cout << "Yes please!" << std::endl;
+        // and much more of if-elses...
+        argc--;
+    }
 }
